9e5572d3368bba29e7b03360555abb3d2eb04b13
Uchiha-Sora <janakiraman0413@gmail.com>
2025-04-27 22:18:52 +0530

    Upadtes to Receive data

--- .ipynb_checkpoints/test-checkpoint.py
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -3,10 +3,8 @@ import requests
 url = "http://127.0.0.1:8000/send_data"
 
 data = {
-    "device id": "Test device 1",
-    "data 1" : 30.4,
-    "data 2" : 40
+    "temperature" : 25.9
 }
 
 response = requests.post(url, json = data)
-print("Server Response:", response.json())
+print("Server Response:", response.text)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Data(BaseModel):
+    temperature: float
 
 @app.get("/")
 
@@ -8,7 +12,6 @@ def read_root():
     return {"message": "IoT Backend is running"}
 
 @app.post("/send_data")
-async def receive_data(request: Request):
-    data = await request.json()
+async def receive_data(data:Data):
     print("Received Data:", data)
     return {"status": "Data received Sucessfully!"}
